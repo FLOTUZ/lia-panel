e6d2984992f580cfd2ef431b630b3fbd8ab97e6d
Michelle Patiño <thocokun@gmail.com>
2022-04-06 17:57:39 -0500

    Crear una nueva aseguradora
    Solo falta ver que show con a tabla

--- pages/aseguradoras/nuevo.tsx
+++ pages/aseguradoras/nuevo.tsx
@@ -19,64 +19,132 @@ import {
     useDisclosure,
     InputLeftElement,
     Stack,
-
+    TableContainer,
+    Table,
+    Thead,
+    Tr,
+    Th,
+    Tbody,
+    Td,
+    Spacer,
+    Heading,
 } from "@chakra-ui/react";
-import {
-    AddIcon, PhoneIcon,
-} from '@chakra-ui/icons'
+import { AddIcon, EmailIcon, PhoneIcon } from "@chakra-ui/icons";
 import React from "react";
-
-
+import { MdVerifiedUser } from "react-icons/md";
+import Link from "next/link";
 
 function AseguradoraNueva() {
-
-    const { isOpen, onOpen, onClose } = useDisclosure()
+    const { isOpen, onOpen, onClose } = useDisclosure();
 
     return (
         <div>
             <DesktopLayout>
                 <Header title={"Crear Nueva Aseguradora"} />
 
-                <Box m={2} bgColor="white" padding={5} borderRadius={10} boxShadow='2xl' p='6' rounded='md' bg='white'>
-
-                    <FormControl isRequired >
-                        <FormLabel htmlFor='nombre'>Nombre</FormLabel>
-                        <Input variant="filled" id='nombre' placeholder='María' />
-
-                    </FormControl>
-
+                <Box
+                    m={2}
+                    bgColor="white"
+                    padding={5}
+                    borderRadius={10}
+                    boxShadow="2xl"
+                    p="6"
+                    rounded="md"
+                    bg="white"
+                >
                     <Stack spacing={4}>
-                        <InputGroup paddingTop={15}>
+                        <InputGroup>
                             <FormControl isRequired>
-                                <FormLabel htmlFor='telefono'>Teléfono</FormLabel>
-                                <InputLeftElement
-                                    pointerEvents='none' paddingTop={45} paddingStart={2} children={<PhoneIcon color='gray.300' />}
-                                />
-                                <Input variant="filled" type='tel' placeholder='4430000000' paddingLeft={8} />
+                                <FormLabel htmlFor="nombre">Nombre de la aseguradora</FormLabel>
+                                <InputGroup>
+                                    <InputLeftElement
+                                        pointerEvents="none"
+                                        children={<MdVerifiedUser color="green" />}
+                                    />
+                                    <Input type="Nombre" placeholder="Qualitas" />
+                                </InputGroup>
+                            </FormControl>
+                        </InputGroup>
+
+                        <InputGroup>
+                            <FormControl isRequired>
+                                <FormLabel htmlFor="email">Correo</FormLabel>
+                                <InputGroup>
+                                    <InputLeftElement
+                                        pointerEvents="none"
+                                        children={<EmailIcon color="gray.300" />}
+                                    />
+                                    <Input type="Email" placeholder="aseguradora@gmail.com" />
+                                </InputGroup>
+                            </FormControl>
+                        </InputGroup>
+
+                        <InputGroup>
+                            <FormControl isRequired>
+                                <FormLabel htmlFor="telefono">Teléfono</FormLabel>
+                                <InputGroup>
+                                    <InputLeftElement
+                                        pointerEvents="none"
+                                        children={<PhoneIcon color="gray.300" />}
+                                    />
+                                    <Input type="tel" placeholder="Phone number" />
+                                </InputGroup>
                             </FormControl>
                         </InputGroup>
                     </Stack>
+                    <Stack marginTop={50} direction="row" spacing={4} align="center">
+                        <Button colorScheme="twitter" variant="solid">
+                            Agregar
+                        </Button>
+
+                        <Link href={"/aseguradoras"}>
+
+                            <a>           <Button colorScheme="red" variant="outline">
+                            Cancelar
+                        </Button>
+                            </a>
+                        </Link>
+                        
+                    </Stack>
+
                 </Box>
 
-
-
-
-                <Box m={2} bgColor="white" padding={5} borderRadius={10} boxShadow='2xl' p='6' rounded='md' bg='white'>
-                    <Button leftIcon={<AddIcon />} colorScheme='teal' variant='solid' onClick={onOpen}>
-
+                <Box
+                    m={2}
+                    bgColor="white"
+                    padding={10}
+                    borderRadius={10}
+                    boxShadow="2xl"
+                    p="6"
+                    rounded="md"
+                    bg="white"
+                >
+                    <Button
+                        leftIcon={<AddIcon />}
+                        colorScheme="teal"
+                        variant="solid"
+                        onClick={onOpen}
+                    >
                         Nueva Asistencia
                     </Button>
+                    <Heading marginTop={50} as="h5" size="md">
+                        Asistencia de aseguradora
+                    </Heading>
+
                     <Modal closeOnOverlayClick={false} isOpen={isOpen} onClose={onClose}>
                         <ModalOverlay />
                         <ModalContent>
                             <ModalHeader>Crea una nueva asistencia</ModalHeader>
                             <ModalCloseButton />
                             <ModalBody pb={6}>
-                                
+                                <FormControl mt={4}>
+                                    <FormLabel>Nombre del servicio</FormLabel>
+                                    <Input placeholder="Nombre del servicio" />
+                                </FormControl>
                             </ModalBody>
 
                             <ModalFooter>
-                                <Button colorScheme='blue' mr={3}>
+                                <Button colorScheme="blue" mr={3}>
                                     Guardar
                                 </Button>
                                 <Button onClick={onClose}>Cancelar</Button>
@@ -84,14 +152,39 @@ function AseguradoraNueva() {
                         </ModalContent>
                     </Modal>
 
+                    <TableContainer>
+                        <Table marginTop={50} size="sm">
+                            <Thead>
+                                <Tr>
+                                    <Th></Th>
+                                    <Th></Th>
+                                    <Th></Th>
+                                </Tr>
+                            </Thead>
+                            <Tbody>
+                                <Tr>
+                                    <Td></Td>
+                                    <Td></Td>
+                                    <Td></Td>
+                                </Tr>
+                                <Tr>
+                                    <Td></Td>
+                                    <Td></Td>
+                                    <Td></Td>
+                                </Tr>
+                                <Tr>
+                                    <Td></Td>
+                                    <Td></Td>
+                                    <Td></Td>
+                                </Tr>
+                            </Tbody>
+                        </Table>
+                    </TableContainer>
 
 
                 </Box>
-
-
-
             </DesktopLayout>
-        </div >
+        </div>
     );
 }
 
