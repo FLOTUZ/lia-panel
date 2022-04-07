f5ffb0b974f287b74ad7def692c9a8c9279fa206
Michelle Patiño <thocokun@gmail.com>
2022-04-06 19:34:52 -0500

    editar aseguradora
    falta implementar bien los edits

--- pages/aseguradoras/index.tsx
+++ pages/aseguradoras/index.tsx
@@ -2,149 +2,225 @@ import Header from "@/common/Header";
 import Link from "next/link";
 import DesktopLayout from "@/layouts/DesktopLayout";
 import {
-  TableContainer, Table, TableCaption, Thead, Tr, Th, Tbody, Td, Tfoot, Box, Button, FormLabel, Input, InputLeftAddon, InputGroup, IconButton, EditableInput, Spacer,
-
+  TableContainer,
+  Table,
+  TableCaption,
+  Thead,
+  Tr,
+  Th,
+  Tbody,
+  Td,
+  Tfoot,
+  Box,
+  Button,
+  FormLabel,
+  Input,
+  InputLeftAddon,
+  InputGroup,
+  IconButton,
+  EditableInput,
+  Spacer,
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalCloseButton,
+  ModalBody,
+  FormControl,
+  ModalFooter,
+  useDisclosure,
 } from "@chakra-ui/react";
 import {
-  AddIcon, AttachmentIcon, EditIcon, PhoneIcon, SearchIcon,
-} from '@chakra-ui/icons'
+  AddIcon,
+  AttachmentIcon,
+  EditIcon,
+  PhoneIcon,
+  SearchIcon,
+} from "@chakra-ui/icons";
 import { useState } from "react";
-import { userInfo } from "os";
-
-
 
 function AseguradorasListado() {
-
+  const { isOpen, onOpen, onClose } = useDisclosure();
   const [query, setQuery] = useState("");
 
-  return <div>Aseguradoras
+  return (
+    <div>
+      Aseguradoras
+      <DesktopLayout>
+        <Header title={"Aseguradoras "} />
+        <Box
+          m={2}
+          bgColor="white"
+          padding={5}
+          borderRadius={10}
+          boxShadow="2xl"
+          p="6"
+          rounded="md"
+          bg="white"
+        >
+          <Box
+            m={2}
+            bgColor="white"
+            padding={5}
+            borderRadius={10}
+            p="6"
+            rounded="md"
+            bg="white"
+          >
+            <Link href={"/aseguradoras/nuevo"}>
+              <a>
+                {" "}
+                <Button
+                  leftIcon={<AddIcon />}
+                  colorScheme="facebook"
+                  variant="solid"
+                  marginLeft={"80%"}
+                >
+                  Nueva aseguradora
+                </Button>
+              </a>
+            </Link>
+          </Box>
 
-    <DesktopLayout>
-      <Header title={"Aseguradoras "} />
-      <Box m={2} bgColor="white" padding={5} borderRadius={10} boxShadow='2xl' p='6' rounded='md' bg='white'>
+          <InputGroup marginLeft={350} marginBlockEnd={10}>
+            <InputLeftAddon>
+              <IconButton
+                disabled
+                aria-label="Search database"
+                icon={<SearchIcon />}
+              />{" "}
+            </InputLeftAddon>
 
+            <Input
+              htmlSize={60}
+              width="auto"
+              type="text"
+              placeholder="Buscar aseguradoras..."
+              className="search"
+            />
+          </InputGroup>
 
-        <Box m={2} bgColor="white" padding={5} borderRadius={10} p='6' rounded='md' bg='white'>
-          <Link href={"/aseguradoras/nuevo"}>
-
-            <a>             <Button leftIcon={<AddIcon />} colorScheme='facebook' variant='solid' marginLeft={'80%'}>Nueva aseguradora</Button >
-            </a>
-          </Link>
-
+          <TableContainer>
+            <Table variant="simple" colorScheme="teal">
+              <TableCaption>Aseguradoras</TableCaption>
+              <Thead>
+                <Tr>
+                  <Th>Nombre</Th>
+                  <Th>Estatus</Th>
+                  <Th>Ultima actividad</Th>
+                  <Th>Asistencias</Th>
+                  <Th>Creado</Th>
+                  <Th>Ultima modificacion</Th>
+                  <Th>Reportes</Th>
+                  <Th></Th>
+                </Tr>
+              </Thead>
+              <Tbody>
+                <Tr>
+                  <Td>Nombre</Td>
+                  <Td>Estatus</Td>
+                  <Td>Actividad</Td>
+                  <Td>
+                    {" "}
+                    5{" "}
+                    <IconButton
+                      onClick={onOpen}
+                      variant="ghost"
+                      aria-label="add"
+                      icon={<AddIcon />}
+                    />
+                  </Td>
+                  <Td>creado</Td>
+                  <Td>Modificacion</Td>
+                  <Td>reportes</Td>
+                  <Td>
+                    <Link href={"/aseguradoras/1"}>
+                      <a>
+                      <IconButton
+                      variant="outline"
+                      aria-label="edit"
+                      icon={<EditIcon />}
+                    />
+                      </a>
+                    </Link>
+                  </Td>
+                </Tr>
+                <Tr>
+                  <Td>Nombre</Td>
+                  <Td>Estatus</Td>
+                  <Td>Actividad</Td>
+                  <Td>
+                    1{" "}
+                    <IconButton
+                      variant="ghost"
+                      onClick={onOpen}
+                      aria-label="add"
+                      icon={<AddIcon />}
+                    />
+                  </Td>
+                  <Td>creado</Td>
+                  <Td>Modificacion</Td>
+                  <Td>reportes</Td>
+                  <Td>
+                    {" "}
+                    <IconButton
+                      variant="outline"
+                      aria-label="edit"
+                      icon={<EditIcon />}
+                    />
+                  </Td>
+                </Tr>
+                <Tr>
+                  <Td>Nombre</Td>
+                  <Td>Estatus</Td>
+                  <Td>Actividad</Td>
+                  <Td>
+                    1{" "}
+                    <IconButton
+                      variant="ghost"
+                      onClick={onOpen}
+                      aria-label="add"
+                      icon={<AddIcon />}
+                    />
+                  </Td>
+                  <Td>creado</Td>
+                  <Td>Modificacion</Td>
+                  <Td>reportes</Td>
+                  <Td>
+                    <IconButton
+                      variant="outline"
+                      aria-label="edit"
+                      icon={<EditIcon />}
+                    />
+                  </Td>
+                </Tr>
+              </Tbody>
+            </Table>
+          </TableContainer>
         </Box>
+        <Modal closeOnOverlayClick={false} isOpen={isOpen} onClose={onClose}>
+          <ModalOverlay />
+          <ModalContent>
+            <ModalHeader>Crea una nueva asistencia</ModalHeader>
+            <ModalCloseButton />
+            <ModalBody pb={6}>
+              <FormControl mt={4}>
+                <FormLabel>Nombre del servicio</FormLabel>
+                <Input placeholder="Nombre del servicio" />
+              </FormControl>
+            </ModalBody>
 
-
-
-        <InputGroup marginLeft={350} marginBlockEnd={10}>
-          <InputLeftAddon>
-            <IconButton
-              disabled
-              aria-label='Search database'
-              icon={<SearchIcon />}
-            />  </InputLeftAddon>
-
-          <Input htmlSize={60} width='auto' type='text' placeholder='Buscar aseguradoras...' className="search" />
-        </InputGroup>
-
-
-        <TableContainer>
-          <Table variant='simple' colorScheme='teal'>
-            <TableCaption>Aseguradoras</TableCaption>
-            <Thead>
-              <Tr>
-                <Th>Nombre</Th>
-                <Th>Estatus</Th>
-                <Th>Ultima actividad</Th>
-                <Th>Asistencias</Th>
-                <Th>Creado</Th>
-                <Th>Ultima modificacion</Th>
-                <Th>Reportes</Th>
-                <Th></Th>
-              </Tr>
-            </Thead>
-            <Tbody>
-              <Tr>
-                <Td>Nombre</Td>
-                <Td>Estatus</Td>
-                <Td>Actividad</Td>
-                <Td> 5 <IconButton
-                  variant='ghost'
-                  aria-label='add'
-                  icon={<AddIcon />}
-                /></Td>
-                <Td>creado</Td>
-                <Td>Modificacion</Td>
-                <Td>reportes</Td>
-                <Td> <IconButton
-
-                  aria-label='Search database'
-                  icon={<EditIcon />}
-                /></Td>
-              </Tr>
-              <Tr>
-                <Td>Nombre</Td>
-                <Td>Estatus</Td>
-                <Td>Actividad</Td>
-                <Td>
-                  1 <IconButton
-                    variant='ghost'
-
-                    aria-label='add'
-                    icon={<AddIcon />}
-                  /></Td>
-                <Td>creado</Td>
-                <Td>Modificacion</Td>
-                <Td>reportes</Td>
-                <Td> <IconButton
-                  variant='outline'
-
-                  aria-label='edit'
-
-                  icon={<EditIcon />}
-
-
-                />
-                </Td>
-
-              </Tr>
-              <Tr>
-                <Td>Nombre</Td>
-                <Td>Estatus</Td>
-                <Td>Actividad</Td>
-                <Td>
-                  1 <IconButton
-                    variant='ghost'
-
-                    aria-label='add'
-                    icon={<AddIcon />}
-                  /></Td>
-                <Td>creado</Td>
-                <Td>Modificacion</Td>
-                <Td>reportes</Td>
-                <Td>
-                   <IconButton
-                  variant='outline'
-
-                  aria-label='edit'
-
-                  icon={<EditIcon />}
-
-
-                />
-                </Td>
-
-              </Tr>
-            </Tbody>
-          </Table>
-        </TableContainer>
-
-      </Box>
-
-
-    </DesktopLayout>
-  </div>;
-
-
+            <ModalFooter>
+              <Button colorScheme="blue" mr={3}>
+                Guardar
+              </Button>
+              <Button onClick={onClose}>Cancelar</Button>
+            </ModalFooter>
+          </ModalContent>
+        </Modal>
+      </DesktopLayout>
+    </div>
+  );
 }
 
 export default AseguradorasListado;
