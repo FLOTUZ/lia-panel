dc4270a593ece04a8e509b1bc697901f43a1f81b
Jaquelyne <jaquelynevazquez1999@gmail.com>
2022-04-26 12:48:08 -0500

    Modificaciones en Ticket

--- pages/tickets/verTicket.tsx
+++ pages/tickets/verTicket.tsx
@@ -336,37 +336,11 @@ function VerTicket() {
                         </Tbody>
                     </Table>
                 </TableContainer>
+                <Button colorScheme="blue" marginLeft={1050}>Editar</Button>
             </Box>
 
 
-            <Modal
-                isOpen={isOpen}
-                onClose={onClose}
-            >
-                <ModalOverlay />
-                <ModalContent>
-                    <ModalHeader>Create your account</ModalHeader>
-                    <ModalCloseButton />
-                    <ModalBody pb={6}>
-                        <FormControl>
-                            <FormLabel>First name</FormLabel>
-                            <Input placeholder='First name' />
-                        </FormControl>
-
-                        <FormControl mt={4}>
-                            <FormLabel>Last name</FormLabel>
-                            <Input placeholder='Last name' />
-                        </FormControl>
-                    </ModalBody>
-
-                    <ModalFooter>
-                        <Button colorScheme='blue' mr={3}>
-                            Save
-                        </Button>
-                        <Button onClick={onClose}>Cancel</Button>
-                    </ModalFooter>
-                </ModalContent>
-            </Modal>
+            
 
         </DesktopLayout >
     );
