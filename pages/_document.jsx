63ebb43ff5ff96dd0457f099f04185b67cc52714
Emmanuel <flotuz10@gmail.com>
2022-05-31 12:08:47 -0500

    SEC

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -1,13 +1,17 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html lang='es'>
+    <Html lang="es">
       <Head />
+      <meta
+        http-equiv="Content-Security-Policy"
+        content="upgrade-insecure-requests"
+      />
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
